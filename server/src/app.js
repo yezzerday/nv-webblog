14f1b8dde78f8b504a2673e4748a033dfe90f80d
yezzerday <jaruda.17502@gmail.com>
2023-08-09 20:12:30 +0700

    page 54

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,16 @@
-console.log('Hello nodejs server')
+let express = require('express')
+const app = express()
+
+app.get('/status', function (req,res){
+    res.send('Hello nodejs server belong to Sarawut')
+})
+
+app.get('/hello/:name', function(req,res){
+    console.log('hello - ' + req.params.name)
+    res.send('say hello with ' + req.params.name)
+})
+
+let port = 8081
+app.listen(port, function(){
+    console.log('server runing on ' + port)
+})
