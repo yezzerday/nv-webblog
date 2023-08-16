ddecdaa1f5d5b34b623e469bd0d7724b1244c8b2
yezzerday <jaruda.17502@gmail.com>
2023-08-16 13:12:29 +0700

    page 82

--- server/src/app.js
+++ server/src/app.js
@@ -1,40 +1,50 @@
-let express = require('express')
-let bodyParser =require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const { sequelize } = require('./models')
 
-const app = express()
+const config = require('./config/config')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+const app = express();
 
-app.get('/status', function (req,res){
-    res.send('Hello nodejs server ')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./route')(app)
+
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server belonging to jaruda');
+});
+
+app.get('/hello/:name', function (req, res) {
+    console.log('hello - ' + req.params.name);
+    res.send('Say hello with ' + req.params.name);
+});
+
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน' + req.params.userId);
+});
+
+app.get('/users', function (req, res) {
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด');
+});
+
+app.post('/user/:userId', function (req, res) {
+    res.send('ทำการสร้างผู้ใช้งาน ' + JSON.stringify(req.body));
+});
+
+app.put('/user/:userId', function (req, res) {
+    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
+});
+
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body));
+});
+
+let port = process.env.PORT || config.port;
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
 })
 
-app.get('/hello/:person', function(req,res){
-    console.log('hello - ' + req.params.person)
-    res.send('say hello with ' + req.params.person)
-})
-
-app.get('/user/:userId', function(req,res){
-    res.send('ดูข้อมูลผู้ใช้งาน:' + req.params.userId)
-})
-
-app.get('/users/', function(req,res){
-    res.send('เรียกข้อมูลผู้ใช้งาน' )
-})
-
-app.post('/user/', function(req,res){
-    res.send('ทำการสร้างผู้ใช้งาน:' + JSON.stringify(req.body))
-})
-
-app.put('/user/:userId', function(req,res){
-    res.send('ทำการแก้ไขผู้ใช้งาน:' + req.params.userId+' : '+JSON.stringify(req.body))
-})
-app.delete('/user/:userId', function(req, res) {
-    res.send('ทำการลบผู้ใช้งาน:' + req.params.userId + ' : ' + JSON.stringify(req.body))
-})
-
-let port = 8081
-app.listen(port, function(){
-    console.log('server runing on ' + port)
-})
